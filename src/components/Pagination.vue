a5c794a5d148460bf6475d9e2cb6e2f9855be835
NatalyaSemenova <ambernou@gmail.com>
2021-10-02 15:58:06 +0700

    Homework for lesson4
    Homework for lesson4

---
+++ src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button
+      v-for="i in pages"
+      :key="i"
+      :class="{ active: curPage === i }"
+      @click="onClickPage(i)"
+    >
+    {{ i }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    pages: {
+      type: Array,
+      default: () => []
+    },
+    curPage: String
+  },
+  methods: {
+    onClickPage (p) {
+      this.$emit('emitPaginate', p)
+      console.log(p)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.active {
+    background: #030303;
+    color: white;
+}
+</style>
