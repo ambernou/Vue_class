35974261481a110e37ec08b677409e4c0c2baae8
NatalyaSemenova <ambernou@gmail.com>
2021-10-14 22:19:51 +0700

    homework for lesson 8
    homework for lesson 8

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from '../views/Dashboard'
+// import DashboardWithPage from '../views/DashboardWithPage'
+import About from '../views/About'
+import NotFound from '../views/NotFound'
+import AddPaymentForm from '../components/AddPaymentForm'
+import Calc from '../components/Calc'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    // {
+    //   path: '/dashboard/:page',
+    //   name: 'DashboardPage',
+    //   component: Dashboard
+    // },
+    {
+    //   path: '/dashboard/:action/:section/:category',
+    //   name: 'AddPaymentFormFromUrl',
+    //   component: Dashboard
+      path: '/add/payment/:category',
+      name: 'AddPaymentForm',
+      component: AddPaymentForm
+    },
+    {
+      path: '/about*',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/calculator',
+      name: 'Calculator',
+      component: Calc
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: NotFound
+    },
+    {
+      path: '*',
+      redirect: '/404'
+    }
+  ]
+})
+
+export default router
---
+++ src/tests/testComponent.test.js
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils'
+import TestComponent from './TestComponent.vue'
+
+describe ('TestComponent testing', () => {
+    test ('test content', () => {
+        const wrapper = mount(TestComponent, {
+            propsData: {
+                message: 'Hello world!'
+            }
+        })
+        expect(wrapper.text()).toEqual('The message is Hello world!')
+    })
+})
